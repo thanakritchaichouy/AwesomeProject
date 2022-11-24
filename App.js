dd8cceaa5e58fdf965c4b83b7a56c9cd79883e52
thanakritchaichouy <114560825+thanakritchaichouy@users.noreply.github.com>
2022-11-24 12:38:22 +0700

    week02

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ex02 from './screens/week02/Ex02';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Ex02/>
+    )
 }
 
 const styles = StyleSheet.create({
